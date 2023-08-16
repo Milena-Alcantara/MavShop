b6f3b7bba85f135ab1417142fadd44b3cab31549
vitoria.ferreira <vitoria.ferreira@zup.com.br>
2023-08-16 19:51:06 -0300

    Adicionando metodo enviarNotificacao no ClientController para envio de boas vindas

--- src/main/kotlin/twilio/TwilioNotifier.kt
+++ src/main/kotlin/twilio/TwilioNotifier.kt
@@ -6,6 +6,7 @@ import com.twilio.rest.api.v2010.account.Message
 import com.twilio.type.PhoneNumber
 
 class TwilioNotifier(private val accountSid: String, private val authToken: String) {
+    private val notifier = TwilioNotifier("AC4a2581c23a5a9f6f37abeedacdbad588", "608f3d1040e13560866ba3d784e59c75")
 
     init {
         Twilio.init(accountSid, authToken)
@@ -17,7 +18,8 @@ class TwilioNotifier(private val accountSid: String, private val authToken: Stri
             PhoneNumber("+17312514152"),
             mensagem
         ).create()
-
+        val numeroDestino = "34996617947"
         println("Notificação enviada com SID: ${message.sid}")
+        notifier.enviarNotificacao(numeroDestino, mensagem)
     }
 }
