cc9fa0a4fd3c2d83bbb57570affac77b01ad501b
Milena.Lopes.Mesquita.Alcântara <milena.alcantara@zup.com.br>
2023-08-16 15:09:03 -0300

    Atualizando funcionalidades voltadas ao cliente

---
+++ src/main/kotlin/controller/ClientController.kt
@@ -0,0 +1,28 @@
+package controller
+
+import model.Client
+import model.ClientRepository
+
+class ClientController {
+    private val clientRepository = ClientRepository()
+    fun addClient(name: String, email: String, password:String, phone: String, cep: String): Boolean {
+        val newClient = Client(name, email, password, phone, cep)
+        clientRepository.addClient(newClient)
+        return true
+    }
+
+    fun loginClient(email: String, password: String): Boolean{
+        while (email.isBlank() || password.isBlank()){
+          return false
+        }
+        clientRepository.loginClient(email,password)
+        return true
+    }
+
+    fun validateClientData(name: String, email: String, password:String, phone: String, cep: String): Boolean{
+        if (name.isNullOrBlank() || email.isNullOrBlank() || password.isNullOrBlank() || phone.isNullOrBlank() || cep.isNullOrBlank()){
+            return false
+        }else return true
+    }
+
+}
