a4d5c9967510f559ea27b1c2d3c0df15a5823dad
vitoria.ferreira <vitoria.ferreira@zup.com.br>
2023-08-16 18:45:01 -0300

    Adicionando lista de produtos preenchida na cesta plus e nos produtos disponiveis

--- src/main/kotlin/model/Cesta.kt
+++ src/main/kotlin/model/Cesta.kt
@@ -17,6 +17,8 @@ class CestaBasica : Cesta(){
         val macarrao = Produto("Macarrão Espaguete - 500g", 3.80, 1)
         val farinhaDeTrigo = Produto("Farinha de Trigo - 1kg", 7.00, 1)
         val milhoPipoca = Produto("Milho de Pipoca - 500g", 3.99, 1)
+        val biscoitoMaizena = Produto("Biscoito Maizena - 200g", 4.00, 2)
+        val molhoPronto = Produto("Molho Fugini - 130g", 2.69, 1)
 
         produtos.add(arroz)
         produtos.add(feijao)
@@ -27,6 +29,8 @@ class CestaBasica : Cesta(){
         produtos.add(macarrao)
         produtos.add(farinhaDeTrigo)
         produtos.add(milhoPipoca)
+        produtos.add(biscoitoMaizena)
+        produtos.add(molhoPronto)
     }
 
     fun calcularValorTotalBasica(): Double {
@@ -40,6 +44,38 @@ class CestaBasica : Cesta(){
 }
 
 class CestaPlus : Cesta(){
+
+    init {
+        val arroz = Produto("Arroz Tipo 1 - 5kg", 12.00, 1)
+        val feijao = Produto("Feijão Carioquinha - 1kg", 6.99, 2)
+        val acucarRefinado = Produto("Açucar refinado - 1kg", 10.00, 2)
+        val salRefinado = Produto("Sal Refinado - 1kg", 6.00, 1)
+        val oleo = Produto("Óleo de soja - 900ml", 4.90, 2)
+        val cafe = Produto("Café - 250g", 11.98, 1)
+        val macarrao = Produto("Macarrão Espaguete - 500g", 3.80, 1)
+        val farinhaDeTrigo = Produto("Farinha de Trigo - 1kg", 7.00, 1)
+        val milhoPipoca = Produto("Milho de Pipoca - 500g", 3.99, 1)
+        val biscoitoMaizena = Produto("Biscoito Maizena - 200g", 4.00, 2)
+        val molhoPronto = Produto("Molho Fugini - 130g", 2.69, 1)
+        val papelHigienico = Produto("Papel Higiênico - 24 rolos", 35.95, 2)
+        val achocolatado = Produto("Achocolatado em pó - 400g", 5.00, 2)
+        val cremeDeLeite = Produto("Creme de leite - 1 lata", 4.00, 2)
+
+        produtos.add(arroz)
+        produtos.add(feijao)
+        produtos.add(acucarRefinado)
+        produtos.add(salRefinado)
+        produtos.add(oleo)
+        produtos.add(cafe)
+        produtos.add(macarrao)
+        produtos.add(farinhaDeTrigo)
+        produtos.add(milhoPipoca)
+        produtos.add(biscoitoMaizena)
+        produtos.add(molhoPronto)
+        produtos.add(papelHigienico)
+        produtos.add(achocolatado)
+        produtos.add(cremeDeLeite)
+    }
     fun adicionarProduto(produto: Produto){
         produtos.add(produto)
     }
