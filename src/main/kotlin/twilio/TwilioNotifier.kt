618f09700d14878ee14b22e64606c8df0cdf33fd
vitoria.ferreira <vitoria.ferreira@zup.com.br>
2023-08-16 21:35:32 -0300

    Adicionando correção no TwilioNotifier

--- src/main/kotlin/twilio/TwilioNotifier.kt
+++ src/main/kotlin/twilio/TwilioNotifier.kt
@@ -10,13 +10,14 @@ class TwilioNotifier(private val accountSid: String, private val authToken: Stri
         Twilio.init(accountSid, authToken)
     }
 
-    fun enviarNotificacao(numeroDestino: String, mensagem: String) {
-        val message = Message.creator(
-            PhoneNumber("numeroDestino"),
-            PhoneNumber(""),
-            mensagem
-        ).create()
+    fun enviarNotificacao(destino: String, mensagem: String) {
+        val numeroDestino = PhoneNumber(destino)
 
-        println("Notificação enviada com SID: ${message.sid}")
+        val remetente = PhoneNumber("SEU_NUMERO_DO_TWILIO")
+
+        val message = Message.creator(numeroDestino, remetente, mensagem).create()
+
+        println("SID da mensagem: ${message.sid}")
     }
 }
+
